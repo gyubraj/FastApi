78834a661392c025ec463f27b3b4532b7272a1e1
Yubraj Upadhaya <gyubraj104@gmail.com>
2022-06-21 14:14:43 +0545

    Single example using Body, Form, Path, Query etc

--- fastapi/request_example.py
+++ fastapi/request_example.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from pydantic import BaseModel, Field
 
 app = FastAPI()
@@ -23,6 +23,15 @@ class Item(BaseModel):
 async def create_items(item: Item):
     return item.dict()
 
+
+@app.post('/single-example-items')
+async def single_example_item(item: Item = Body(example={
+                "name": "Book",
+                "description": "Book is written by Yubraj Upadhaya.",
+                "price": 45.3,
+                "tax": 12.3
+            })):
+            return item.dict()
 
 
 
