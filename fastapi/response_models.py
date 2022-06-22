93a51f6976f01fa6413337375e96b106a2d1fd80
Yubraj Upadhaya <gyubraj104@gmail.com>
2022-06-22 16:39:07 +0545

    Include and Exclude in response model

--- fastapi/response_models.py
+++ fastapi/response_models.py
@@ -9,7 +9,7 @@ class Item(BaseModel):
     name: str
     description: str | None = None
     price: float
-    tax: float | None = None
+    tax: float  = 10.5
     tags: list[str] = []
 
 
@@ -37,3 +37,23 @@ async def create_wrong_user(user: UserIn):
 @app.post("/right-user/", response_model=UserOut)
 async def create_right_user(user: UserIn):
     return user
+
+
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+}
+
+@app.get('/items/{item_id}', response_model=Item,response_model_exclude_unset= True)
+async def read_items(item_id: str):
+    return items[item_id]
+
+@app.get('/include-items/{item_id}', response_model=Item, response_model_include=['name','price'])
+async def read_items(item_id: str):
+    return items[item_id]
+
+
+@app.get('/exclude-items/{item_id}', response_model=Item, response_model_exclude=['description','price'])
+async def read_items(item_id: str):
+    return items[item_id]
