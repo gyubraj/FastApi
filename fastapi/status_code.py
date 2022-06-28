d29fae9f9c1ac6e7eef450bda76650f833be1cad
Yubraj Upadhaya <gyubraj104@gmail.com>
2022-06-28 14:53:52 +0545

    Status code in path decorator

---
+++ fastapi/status_code.py
@@ -0,0 +1,12 @@
+from turtle import clear
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+@app.post("/items/",status_code= 201)
+
+async def create_item(name: str):
+    return {
+        'name': name
+    }
