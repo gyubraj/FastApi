4c1178b8a2d1733b81e56954fef050407a5c5571
Yubraj Upadhaya <gyubraj104@gmail.com>
2022-06-28 14:59:48 +0545

    Http Status code

--- fastapi/status_code.py
+++ fastapi/status_code.py
@@ -1,11 +1,18 @@
-from turtle import clear
-from fastapi import FastAPI
+
+from fastapi import FastAPI, status
 
 
 app = FastAPI()
 
 @app.post("/items/",status_code= 201)
+async def create_item(name: str):
+    return {
+        'name': name
+    }
 
+# HTTP status codes
+
+@app.post("/create-item/", status_code= status.HTTP_201_CREATED)
 async def create_item(name: str):
     return {
         'name': name
